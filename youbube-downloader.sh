37b373f5e8cc404c2fa8ce5069245b98378e1792
unknown <Vincent Buzzano>
2020-08-17 16:59:17 +0200

    add print function and default values

--- youbube-downloader.sh
+++ youbube-downloader.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
+DOWNLOAD_SPEED_LIMIT=20M
 DEST_DIR=/media/rdc-nas/video/Youtube
 DOWNLOAD_ARCHIVE=$DEST_DIR/downloaded.txt
 BATCH_FILE=$DEST_DIR/channel_list.txt
 COOKIES_FILE=$DEST_DIR/google_cookies.txt
 LOG_FILE=$DEST_DIR/downloaded.log
-
-DOWNLOAD_SPEED_LIMIT=20M
-SUB_LANG=fr,en
-SUB_FORMAT=srt
-FORMAT='bestvideo[ext=?mp4][height<=?720]+bestaudio[ext=?mp3]/best'
-DATE_AFTER=$(date +"%Y0101")
-#DATE_AFTER="now-2weekss"
-TMP_DIR=/tmp/youtube-downloader
-
 CACHE_DIR=$DEST_DIR/cache
 
+DEFAULT_DATEAFTER=$(date +"%Y0101")
+DEFAULT_SUBTITLELANG=""
+DEFAULT_SUBTITLEFORMAT="srt"
+DEFAULT_FORMAT="bestvideo[ext=?mp4][height<=?720]+bestaudio[ext=?mp3]/best"
+DEFAULT_MAXFILESIZE="2048m"
+
+TMP_DIR=/tmp/youtube-downloader
 CURRENT_DIR=`pwd`
 
+# assure download archive exists
 touch $DOWNLOAD_ARCHIVE
+
+# init cache dir
+mkdir -p $CACHE_DIR
+
 
 _append_past_videos_to_downloaded_archive() {
   # $1 = channel url
@@ -27,7 +31,7 @@ _append_past_videos_to_downloaded_archive() {
   FILECACHE=$2
   youtube-dl --playlist-reverse \
     -i -s \
-    --datebefore $DATE_AFTER \
+    --datebefore $DEFAULT_DATEAFTER \
     --no-cache-dir \
     --cookies $COOKIES_FILE \
     --youtube-skip-dash-manifest \
@@ -35,6 +39,10 @@ _append_past_videos_to_downloaded_archive() {
     $URL | tee $FILECACHE
     sed -i -E "s/(.*)/youtube \1/" $FILECACHE
     cat $FILECACHE >> $DOWNLOAD_ARCHIVE && sed -i '/^$/d' $DOWNLOAD_ARCHIVE
+}
+
+_print() {
+  echo "$@" >$LOG_FILE 2>&1
 }
 
 _download_videos() {
@@ -49,15 +57,15 @@ _download_videos() {
   youtube-dl --playlist-reverse \
     --download-archive ../downloaded.txt \
     -i \
-    -f $FORMAT \
-    --dateafter $DATE_AFTER \
+    -f $DEFAULT_FORMAT \
+    --dateafter $DEFAULT_DATEAFTER \
     --youtube-skip-dash-manifest \
     --autonumber-start 0001 \
     --no-cache-dir \
     --cookies $COOKIES_FILE \
     --add-metadata \
     --write-thumbnail \
-    --write-sub --write-auto-sub --sub-lang $SUB_LANG --sub-format $SUB_FORMAT --convert-subs $SUB_FORMAT \
+    --write-sub --write-auto-sub --sub-lang $DEFAULT_SUBTITLELANG --sub-format $DEFAULT_SUBTITLEFORMAT --convert-subs $DEFAULT_SUBTITLEFORMAT \
     --write-description \
     --limit-rate $DOWNLOAD_SPEED_LIMIT \
     -o "%(uploader)s/%(uploader)s - S$(date +"%Y")/%(playlist_index)s. %(title)s %(upload_date)s/%(uploader)s - S$(date +"%Y")E%(playlist_index)s - %(upload_date)s - %(title)s --end.%(ext)s" \
@@ -65,10 +73,10 @@ _download_videos() {
   COUNT=$(find . -mindepth 1 -maxdepth 1 -type d | wc -l)
   if [ $COUNT -gt 0 ]; then
     rename 's/(.*) - (\d{4})(\d{2})(\d{2}) - (.*) --end\.(.*)/$1 - $2-$3-$4 - $5.$6/' **/**/**/*.* && \
-    echo "Copy downloaded videos to $DEST_DIR" >log.txt 2>&1 && \
+    _print "Copy downloaded videos to $DEST_DIR" && \
     cp -rfn * $DEST_DIR && \
     cd .. && \
-    echo "Append downloaded archive to $DOWNLOAD_ARCHIVE and remove empty lines" >log.txt 2>&1 && \
+    _print "Append downloaded archive to $DOWNLOAD_ARCHIVE and remove empty lines" && \
     grep -Fxvf $DOWNLOAD_ARCHIVE ./downloaded.txt >> $DOWNLOAD_ARCHIVE && sed -i '/^$/d' $DOWNLOAD_ARCHIVE
   fi
   cd $CURRENT_DIR && \
@@ -76,29 +84,27 @@ _download_videos() {
 }
 
 
-# init cache dir
-mkdir -p $CACHE_DIR
-
 # Loop batch file
 awk '!/^(#|;|\])/'  $BATCH_FILE |
 while IFS= read -r URL; do
 
-  echo "" >log.txt 2>&1
-  echo "Prepare to download videos for $URL ..." >log.txt 2>&1
+  _print ""
+  _print "Prepare to download videos for $URL ..."
 
   # check if we need to add old videos to downloaded archive
   KEY=$(echo -n "$URL" | md5sum | cut -d' ' -f1)
   filename="$CACHE_DIR/$KEY.txt"
   if [ ! -f "$filename" ]; then
-      echo "" >log.txt 2>&1
-      echo "Add past videos id to download archive for $URL to file $filename ..."
-      _append_past_videos_to_downloaded_archive $URL $filename >log.txt 2>&1
+      _print
+      _print "Add past videos id to download archive for $URL to file $filename ..."
+      _append_past_videos_to_downloaded_archive $URL $filename
   fi
 
-  echo "" >log.txt 2>&1
-  echo "Download new videos files for $URL" >log.txt 2>&1
+  _print
+  _print "nDownload new videos files for $URL"
+  
   _download_videos $URL
   
-  echo ""
-  echo "done."
+  _print
+  _print "done."
 done
